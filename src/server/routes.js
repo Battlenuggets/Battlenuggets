08794225d8cbcfec7bd0bc6f481bc6a6d1d16ec6
Patrick Truong <pahtruon@gmail.com>
2016-07-14 16:28:51 -0700

    Fixed merged

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,4 +1,3 @@
-<<<<<<< d22a9f424b37e9ccf473e301b912cdf8424426d0
 var userRoutes = require('./users/userRoutes')
 
 module.exports = function (app, express) {
@@ -8,15 +7,4 @@ module.exports = function (app, express) {
   app.get('/api/users/signedin', userRoutes.checkAuth);
 
 };
-||||||| merged common ancestors
-=======
-var userRoutes = require('./users/userRoutes.js');
 
-module.exports = function (app, express) {
-
-  app.post('/api/users/signin', userRoutes.signin);
-  app.post('/api/users/signup', userRoutes.signup);
-  app.get('/api/users/signedin', userRoutes.checkAuth);
-
-};
->>>>>>> Added server stuff
