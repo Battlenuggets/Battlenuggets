006d51bd8e46c869b20aa867b39996d7c60a1f22
Patrick Truong <pahtruon@gmail.com>
2016-07-14 11:14:08 -0700

    Added server stuff

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,3 +1,4 @@
+<<<<<<< d22a9f424b37e9ccf473e301b912cdf8424426d0
 var userRoutes = require('./users/userRoutes')
 
 module.exports = function (app, express) {
@@ -7,3 +8,15 @@ module.exports = function (app, express) {
   app.get('/api/users/signedin', userRoutes.checkAuth);
 
 };
+||||||| merged common ancestors
+=======
+var userRoutes = require('./users/userRoutes.js');
+
+module.exports = function (app, express) {
+
+  app.post('/api/users/signin', userRoutes.signin);
+  app.post('/api/users/signup', userRoutes.signup);
+  app.get('/api/users/signedin', userRoutes.checkAuth);
+
+};
+>>>>>>> Added server stuff
