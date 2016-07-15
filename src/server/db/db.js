bd45a763872f8af73d6d3c69ce4b9df188896c1e
budleigh <arguewithplato@gmail.com>
2016-07-15 10:29:43 -0700

    (refactor) magic strings cleanup in db

--- src/server/db/db.js
+++ src/server/db/db.js
@@ -1,7 +1,11 @@
 var Sequelize = require('sequelize');
-var url = process.env.DATABASE_URL || 'localhost';
 
-var sequelize = new Sequelize('d414dlcug3njlv', 'dvxtkxwgylbtrz', 'OkJpl2NmJfYhnlOSTjAepdXN29', {
+var url = process.env.DATABASE_URL || 'localhost';
+var dbname = 'd414dlcug3njlv';
+var username = 'dvxtkxwgylbtrz';
+var password = 'OkJpl2NmJfYhnlOSTjAepdXN29'
+
+var sequelize = new Sequelize(dbname, username, password, {
   host: url,
   dialect: 'postgres'
 });
