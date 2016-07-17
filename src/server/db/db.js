767cd8f0e74e59262ca57282cd86397e74c03c2e
budleigh <arguewithplato@gmail.com>
2016-07-16 21:51:50 -0700

    (fix) local ssl fix for pg access

--- src/server/db/db.js
+++ src/server/db/db.js
@@ -1,15 +1,28 @@
 var Sequelize = require('sequelize');
 
+var dbname = 'daghgmnrb6tjeh';
+var username = 'temhxbwqyzuvgl';
+var password = 'ShZB_n-4FIZVt9SSvZ0aHix-je';
 var url = process.env.DATABASE_URL || 
-  'postgres://temhxbwqyzuvgl:ShZB_n-4FIZVt9SSvZ0aHix-je@ec2-107-22-235-119.compute-1.amazonaws.com:5432/daghgmnrb6tjeh';
+  `postgres://${username}:${password}@localhost:5432/${dbname}`;
 
-var sequelize = new Sequelize(url, {
-  'ssl': true,
-  'dialectOptions': {
-    'ssl': {
-      'require': true
+/**
+ * heroku requires ssl for pg conns, so
+ * if database_url is set (were on heroku)
+ * we set ssl, otherwise just pass the
+ * url for local repos and pg
+ */
+var options = process.env.DATABASE_URL ?
+  {
+    'ssl': true,
+    'dialectOptions': {
+      'ssl': {
+        'require': true
+      }
     }
-  }
-});
+  } :
+  {};
+
+var sequelize = new Sequelize(url, options);
 
 module.exports = sequelize;
