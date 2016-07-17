3a1a703bbf5f5a740b829b9dc9c2911c4ad46e82
budleigh <arguewithplato@gmail.com>
2016-07-17 10:57:59 -0700

    (refactor) linting settings

--- src/server/settings.js
+++ src/server/settings.js
@@ -1,5 +1,5 @@
 exports.DBNAME = 'nugget_test';
 exports.USERNAME = 'yoozer';
 exports.PASSWORD = 'bumblebee';
-exports.DBURL = process.env.DATABASE_URL || 
+exports.DBURL = process.env.DATABASE_URL ||
   `postgres://${exports.USERNAME}:${exports.PASSWORD}@localhost:5432/${exports.DBNAME}`;
