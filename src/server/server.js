839b92abc9b784984bdf745c485fa394a2ae3fd6
Patrick Truong <pahtruon@gmail.com>
2016-07-17 15:26:09 -0700

    (refactor) Change server code to handle sockets

--- src/server/server.js
+++ src/server/server.js
@@ -5,11 +5,14 @@ var db = require('./db/db');
 var path = require('path');
 
 var app = express();
+var server = require('http').createServer(app); //added for sockets
+var io = require('socket.io').listen(server); //added for sockets
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/../client')));
+// app.use(express.static(path.join(__dirname + '/../client/chat')));
 
 require('./routes.js')(app, express);
 
@@ -19,8 +22,16 @@ var port = process.env.PORT || 8000;
 // db initialization is a prereq of the app
 // being launched, so we chain it up
 db.sync().then(function () {
-  app.listen(port);
+  server.listen(port);
+  // app.listen(port);
   console.log('listening to 8000');
+});
+
+//listen on the connection even for incoming sockets
+io.sockets.on('connection', function (socket) {
+  socket.on('send msg', function (data) {
+    io.sockets.emit('get msg', data);
+  });
 });
 
 // export our app for testing and flexibility, required by index.js
