ae879984d1133b043b5e52bd86dffad3e4ea2ff0
budleigh <arguewithplato@gmail.com>
2016-07-17 10:56:10 -0700

    (feat) added global sorta settings file

---
+++ src/server/settings.js
@@ -0,0 +1,5 @@
+exports.DBNAME = 'nugget_test';
+exports.USERNAME = 'yoozer';
+exports.PASSWORD = 'bumblebee';
+exports.DBURL = process.env.DATABASE_URL || 
+  `postgres://${exports.USERNAME}:${exports.PASSWORD}@localhost:5432/${exports.DBNAME}`;
