f41f9b4dc75beca880b74aa97f2441028831673d
Forrest Labrum <forrest.labrum@gmail.com>
2016-07-18 16:54:59 -0600

    (test) add sign-in test for incorrect password

--- test/server/users/userRoutes.js
+++ test/server/users/userRoutes.js
@@ -3,20 +3,20 @@ var request = require('supertest');
 var User = require('../../../src/server/users/userModel');
 var app = require('../../../src/server/server');
 
+var testUser = {
+  userId: 'iamatestuser',
+  password: 'supersecret',
+  currency: 0
+}
+
 describe('Auth API', function () {
 
   beforeEach(function () {
-    var testUser = {
-      userId: 'iamatestuser',
-      password: 'supersecret',
-      currency: 0
-    }
-
     User.create(testUser);
   });
 
   describe('Sign-In', function () {
-    it('should respond with a 404 if user not found', function() {
+    it('should respond with a 404 if user not found', function(done) {
       var nonexistentUser = {
         username: 'billyjofosho',
         password: 'letmein'
@@ -25,7 +25,19 @@ describe('Auth API', function () {
       request(app)
         .post('/api/users/signin')
         .send(nonexistentUser)
-        .expect(404);
+        .expect(404, done);
+    });
+
+    it('should respond with a 401 if wrong password is used', function(done) {
+      var wrongPassword = {
+        username: 'iamatestuser',
+        password: 'badPassword',
+      }
+
+      request(app)
+        .post('/api/users/signin')
+        .send(wrongPassword)
+        .expect(401, done);
     });
   });
 });
