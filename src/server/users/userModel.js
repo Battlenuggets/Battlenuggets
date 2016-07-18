d23cd6b4a670a628819cfdcef6ee59c801ae8fb8
Forrest Labrum <forrest.labrum@gmail.com>
2016-07-18 15:54:40 -0600

    (feat) add bcrypt functionality to the auth service

--- src/server/users/userModel.js
+++ src/server/users/userModel.js
@@ -1,4 +1,6 @@
 var Sequelize = require('sequelize');
+var Promise = require('bluebird');
+var bcrypt = Promise.promisifyAll(require('bcrypt-nodejs'));
 var db = require('../db/db');
 
 var User = db.define('user', {
@@ -8,5 +10,17 @@ var User = db.define('user', {
 }, {
   timestamps: false  // for later debate
 });
+
+User.hook('beforeCreate', function(user) {
+  return bcrypt.hashAsync(user.password, null, null)
+    .then(function (hash) {
+      user.password = hash;
+      console.log(user.password);
+  });
+})
+
+User.comparePasswords = function(possPassword, currPassword) {
+  return bcrypt.compareAsync(possPassword, currPassword);
+}
 
 module.exports = User;
