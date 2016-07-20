5dbc90c0f1ae57a54d4747cdb128bec30087d35a
graftss <coughcoughcough@gmail.com>
2016-07-20 18:36:03 -0400

    (feat) simplify `Fighter` class

--- src/server/game/fighter.js
+++ src/server/game/fighter.js
@@ -1,19 +1,22 @@
 var _ = require('lodash');
 
-function Fighter (team) {
-  // each fighter should know which team they're on
-  this.team = team;
-
+function Fighter () {
   // combat stats
   this.health = 100;
   this.attack = 10;
-  this.defense = 3;
-  this.targets = 1;
 }
 
 Fighter.prototype.getDamageRoll = function () {
   // pokemon style damage ranges
   return this.attack * _.random(0.85, 1.0);
+};
+
+Fighter.prototype.takeDamage = function (damage) {
+  this.health -= damage;
+};
+
+Fighter.prototype.isDead = function () {
+  return this.health <= 0;
 };
 
 module.exports = Fighter;
