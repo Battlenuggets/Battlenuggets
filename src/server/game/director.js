f975ef17f0b192be31c3ee3d9821d92b9c1324a0
graftss <coughcoughcough@gmail.com>
2016-07-21 12:02:03 -0400

    (feat) add emitter to `Director`

--- src/server/game/director.js
+++ src/server/game/director.js
@@ -1,28 +1,48 @@
-var Battle = require('./battle');
-var Team = require('./team');
-var Fighter = require('./fighter');
+var ee = require('event-emitter');
+
+var tickEvent = 'tick';
+var endOfBattleEvent = 'endOfBattle';
 
 // eventually, would be nice to plug in `battleConfig` or something as an
 // argument, so that the director can set up the `Battle` instance itself
-function Director (battle) {
+function Director (battle, tickInterval) {
   this.battle = battle;
+  this.tickInterval = tickInterval;
+
   this.round = 1;
+  this.emitter = ee();
 }
 
-Director.prototype.tick = function () {
-  // don't tick if the battle's over
-  // return `null` to let the caller know
-  if (this.battle.isEnded()) return null;
+Director.prototype.startBattle = function () {
+  this.tickTimeout = setInterval(this.tick.bind(this), this.tickInterval);
+};
 
-  var attackOrder = battle.generateAttackOrder();
+Director.prototype.onTick = function (callback) {
+  this.emitter.on(tickEvent, callback);
+};
+
+Director.prototype.onEndOfBattle = function (callback) {
+  this.emitter.on(endOfBattleEvent, callback);
+};
+
+// execute a round of battle, where every nugget attacks some other one
+// in a random order
+Director.prototype.tick = function () {
+  var attackOrder = this.battle.generateAttackOrder();
 
   // this is what we'd want to send over the socket to be rendered client-side
-  var attackActions = battle.generateAttackActions(attackOrder);
+  var attackActions = this.battle.generateAttackActions(attackOrder);
 
-  battle.executeAttackActions(attackActions);
+  this.battle.executeAttackActions(attackActions);
   this.round += 1;
 
-  return attackActions;
+  // if the battle's over, emit `endOfBattleEvent` and stop ticking
+  if (this.battle.isEnded()) {
+    this.emitter.emit(endOfBattleEvent, this.battle.getEndOfBattleData());
+    clearTimeout(this.tickTimeout);
+  } else {
+    this.emitter.emit(tickEvent, attackActions);
+  }
 };
 
 module.exports = Director;
