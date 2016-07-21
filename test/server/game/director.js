e0398f92e252302e9c66e78451697bbe8eee2ce9
graftss <coughcoughcough@gmail.com>
2016-07-21 12:20:39 -0400

    (test) add `Director` test for tick emitter

--- test/server/game/director.js
+++ test/server/game/director.js
@@ -6,10 +6,11 @@ var getMockBattle = require('./mockBattle');
 describe('Director', function () {
   var battle;
   var director;
+  var tickInterval = 30;
 
   beforeEach(function () {
     battle = getMockBattle().battle;
-    director = new Director(battle);
+    director = new Director(battle, tickInterval);
   });
 
   it('should tick the battle forward', function () {
@@ -27,5 +28,25 @@ describe('Director', function () {
     });
 
     expect(aNuggetWasHit).to.equal(true);
+  });
+
+  it('should notify listeners on tick', function (done) {
+    var battleLog = [];
+    var callback = function (data) {
+      battleLog.push(data);
+    };
+
+    director.onTick(callback);
+    director.startBattle();
+
+    // there should have been two ticks after `tickInterval * 2.5` ms
+    setTimeout(function () {
+      expect(battleLog.length).to.equal(2);
+
+      // each entry in the log should have 6 attacks
+      expect(battleLog[0].length).to.equal(6);
+      expect(battleLog[1].length).to.equal(6);
+      done();
+    }, tickInterval * 2.5);
   });
 });
