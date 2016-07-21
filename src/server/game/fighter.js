100dd4bb2fbb964d543a3c6b6f989cadceab42c3
graftss <coughcoughcough@gmail.com>
2016-07-20 20:04:39 -0400

    (feat) add functionality to `Fighter`

--- src/server/game/fighter.js
+++ src/server/game/fighter.js
@@ -1,14 +1,23 @@
 var _ = require('lodash');
 
-function Fighter () {
+function Fighter (team) {
   // combat stats
   this.health = 100;
   this.attack = 10;
+
+  // record the fighter's team
+  this.team = team;
 }
 
 Fighter.prototype.getDamageRoll = function () {
   // pokemon style damage ranges
   return this.attack * _.random(0.85, 1.0);
+};
+
+Fighter.prototype.attack = function (defender) {
+  var damage = this.getDamageRoll();
+
+  defender.takeDamage(damage);
 };
 
 Fighter.prototype.takeDamage = function (damage) {
