89b4d0a4f2cb9625d4d97deb02169fc0cf121308
budleigh <arguewithplato@gmail.com>
2016-07-22 16:06:53 -0700

    (feat) basic store controller

--- src/client/app/store/store.js
+++ src/client/app/store/store.js
@@ -1,4 +1,45 @@
 angular.module('battle.store', [])
 
-.controller('StoreController', function ($scope) {
+.controller('StoreController', function ($scope, Store) {
+  $scope.wallet = 0;
+  $scope.items = [
+    {
+      name: 'nugget icon',
+      price: 15
+    },
+    {
+      name: 'nuggetlord icon',
+      price: 50
+    },
+    {
+      name: 'nuggetgod icon',
+      price: 500
+    }
+  ];
+  $scope.inventory = [];
+
+  $scope.purchase = function (item) {
+    Store.purchase(item)
+      .then(function () {
+        alert('purchased!');
+        $scope.updateCurrency();
+      });
+  }
+
+  $scope.updateCurrency = function () {
+    Store.getCurrency()
+      .then(function (currency) {
+        $scope.wallet = currency;
+      });
+  }
+
+  $scope.updateInventory = function () {
+    Store.getInventory()
+      .then(function (inventory) {
+        $scope.inventory = inventory;
+      });
+  }
+
+  $scope.updateCurrency();
+  $scope.updateInventory();
 });
