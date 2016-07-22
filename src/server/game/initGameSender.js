bd7c55fb5d0567e45f3bc28d84604868dc6a153a
graftss <coughcoughcough@gmail.com>
2016-07-22 16:22:48 -0400

    (feat) add `initGameSender` to send game state data to clients

---
+++ src/server/game/initGameSender.js
@@ -0,0 +1,26 @@
+var Director = require('./director');
+
+function initGameSender (io) {
+  this.io = io;
+
+  // use local one for now, there's no reason to interact with it elsewhere
+  var director = new Director(1000, 1000);
+
+  // queue up the first battle
+  director.queueNextBattle();
+
+  // hook up event callbacks to send data to all connected clients
+  director.onStartOfBattle(createSender(io, 'start of battle'));
+  director.onTick(createSender(io, 'tick'));
+  director.onEndOfBattle(createSender(io, 'end of battle'));
+}
+
+// returns a callback function that sends `data` to all sockets
+// that are connected to `io`
+function createSender (io, event) {
+  return function (data) {
+    io.sockets.emit(event, data);
+  };
+}
+
+module.exports = initGameSender;
