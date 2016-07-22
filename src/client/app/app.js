f0acae6fc11e14b50a814ea5729120197f06f5fd
budleigh <arguewithplato@gmail.com>
2016-07-22 14:06:32 -0700

    (feat) add store routes

--- src/client/app/app.js
+++ src/client/app/app.js
@@ -7,7 +7,8 @@ angular.module('battle', [
   'betboard',
   'luegg.directives',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'battle.store',
 ])
 
 .config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
@@ -30,6 +31,12 @@ angular.module('battle', [
           controller: 'ChatCtrl'
         }
       }
+    })
+    .state('store', {
+      url: '/store',
+      templateUrl: 'app/store/store.html',
+      controller: 'StoreController',
+      authenticate: true
     })
     .state('main', {
       url: '/main',
