0b2394a5d0d2a7456fc391362125afa421c457e8
graftss <coughcoughcough@gmail.com>
2016-07-22 22:06:30 -0400

    (refactor) move gamestate listener logic out of `GameCtrl`

--- src/client/app/game/game.js
+++ src/client/app/game/game.js
@@ -1,16 +1,41 @@
 angular.module('battle.game', [])
-  .controller('GameCtrl', function ($scope, socket) {
+  .factory('gameListeners', function () {
+    var startOfBattleEvent = 'start of battle';
+    var tickEvent = 'tick';
+    var endOfBattleEvent = 'end of battle';
+
+    function onStartOfBattle (data) {
+      console.log('start', data);
+    }
+
+    function onTick (data) {
+      console.log('tick', data);
+    }
+
+    function onEndOfBattle (data) {
+      console.log('end', data);
+    }
+
+    return {
+      remove: function (socket) {
+        socket.removeAllListeners(startOfBattleEvent);
+        socket.removeAllListeners(tickEvent);
+        socket.removeAllListeners(endOfBattleEvent);
+      },
+
+      add: function (socket) {
+        // remove the existing listeners on the gamestate events so
+        // that we don't have duplicate listeners
+        this.remove(socket);
+
+        socket.on(startOfBattleEvent, onStartOfBattle);
+        socket.on(tickEvent, onTick);
+        socket.on(endOfBattleEvent, onEndOfBattle);
+      }
+    };
+  })
+  .controller('GameCtrl', ['$scope', 'socket', 'gameListeners', function ($scope, socket, gameListeners) {
     $scope.woop = 'sjsjsjs';
 
-    socket.on('start of battle', function (data) {
-      console.log('start', data);
-    });
-
-    socket.on('tick', function (data) {
-      console.log('tick', data);
-    });
-
-    socket.on('end of battle', function (data) {
-      console.log('end', data);
-    });
-  });
+    gameListeners.add(socket);
+  }]);
